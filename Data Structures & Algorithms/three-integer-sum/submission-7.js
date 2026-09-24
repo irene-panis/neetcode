8852737dfbc9ec02c7856961eb58f3e131bb512a
Irene <panisirene@gmail.com>
2026-09-24 01:26:59 -0700

    Add: three-integer-sum - submission-7

---
+++ Data Structures & Algorithms/three-integer-sum/submission-7.js
@@ -0,0 +1,34 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[][]}
+     */
+    threeSum(nums) {
+        let res = [];
+        const sorted = nums.sort((a, b) => a - b);
+
+        for (let i = 0; i < sorted.length; i++) {
+            if (sorted[i] === sorted[i - 1]) continue;
+            let l = i + 1;
+            let r = sorted.length - 1;
+            while (l < r) {
+                if (sorted[i] + sorted[l] + sorted[r] > 0) {
+                    r--;
+                } else if (sorted[i] + sorted[l] + sorted[r] < 0) {
+                    l++;
+                } else {
+                    res.push([sorted[i], sorted[l], sorted[r]]);
+                    l++;
+                    r--;
+                    while (sorted[l] == sorted[l - 1]) {
+                        l++;
+                    }
+                    while (sorted[r] == sorted[r + 1]) {
+                        r--;
+                    }
+                }
+            }
+        }
+        return res;
+    }
+}
