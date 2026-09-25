7e14988e920b6304f1d949ed245ded32c92c3cb4
Irene <panisirene@gmail.com>
2026-09-25 13:22:49 -0700

    Add: car-fleet - submission-4

---
+++ Data Structures & Algorithms/car-fleet/submission-4.js
@@ -0,0 +1,27 @@
+class Solution {
+    /**
+     * @param {number} target
+     * @param {number[]} position
+     * @param {number[]} speed
+     * @return {number}
+     */
+    carFleet(target, position, speed) {
+        const cars = new Map();
+        const stack = [];
+
+        for (let i = 0; i < position.length; i++) {
+            cars.set(position[i], speed[i]);
+        }
+
+        const sortedCars = [...cars].sort((a, b) => b[0] - a[0]);
+
+        for (let i = 0; i < sortedCars.length; i++) {
+            const time = ((target - sortedCars[i][0]) / sortedCars[i][1]);
+            stack.push(time);
+            if (stack.length >= 2 && stack[stack.length - 1] <= stack[stack.length - 2]) {
+                stack.pop();
+            }
+        }
+        return stack.length;
+    }
+}
