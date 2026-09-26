798e8aa13d94440f5b514c8c0e04aee584e4f9b5
Irene <panisirene@gmail.com>
2026-09-25 20:14:26 -0700

    Add: daily-temperatures - submission-8

---
+++ Data Structures & Algorithms/daily-temperatures/submission-8.js
@@ -0,0 +1,19 @@
+class Solution {
+    /**
+     * @param {number[]} temperatures
+     * @return {number[]}
+     */
+    dailyTemperatures(temperatures) {
+        let res = new Array(temperatures.length).fill(0);
+        const stack = [];
+
+        for (let i = 0; i < temperatures.length; i++) {
+            while (temperatures[i] > temperatures[stack[stack.length - 1]]) {
+                const prev = stack.pop();
+                res[prev] = i - prev;
+            }
+            stack.push(i);
+        }
+        return res;
+    }
+}
